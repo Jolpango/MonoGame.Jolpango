eb516d3d3e78fc938cf52898edb125009c4c0eba
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:02 +0200

    Create JComponent.cs

---
+++ Components/JComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Components
+{
+    public abstract class JComponent
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public List<string> Tags { get; set; }
+        public Game Game { get; private set; }
+        public JComponent(Game game)
+        {
+            Game = game;
+            Id = Guid.NewGuid().ToString();
+            Tags = new List<string>();
+        }
+
+        public void LoadContent()
+        {
+
+        }
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
