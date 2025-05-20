01f3a0eb977f15834fc2d3eca6f1294445fc750e
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:33:17 +0200

    Create TileSet.cs

---
+++ Tiled/TileSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Tiled
+{
+    public class TileSet
+    {
+        public int FirstGid { get; set; }
+        public string Source { get; set; }
+    }
+}
