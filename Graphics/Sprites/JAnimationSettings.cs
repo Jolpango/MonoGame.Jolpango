145afc90b2f2cc37730551f72d806c77f1195459
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:22 +0200

    Create JAnimationSettings.cs

---
+++ Graphics/Sprites/JAnimationSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Graphics.Sprites
+{
+    public class JAnimationSettings
+    {
+        public JTextureAtlasSettings TextureAtlas { get; set; }
+        public Dictionary<string, JAnimationCycleSettings> Cycles { get; set; }
+    }
+
+    public class JTextureAtlasSettings
+    {
+        public string Texture { get; set; }
+        public int RegionWidth { get; set; }
+        public int RegionHeight { get; set; }
+    }
+
+    public class JAnimationCycleSettings
+    {
+        public int[] Frames { get; set; }
+        public bool IsLooping { get; set; }
+        public float FrameDuration { get; set; }
+    }
+}
