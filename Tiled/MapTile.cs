c785e4837c9f88ecde81490f3892779fd802f3c1
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:33:16 +0200

    Create MapTile.cs

---
+++ Tiled/MapTile.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Jolpango.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Tiled
+{
+    public class MapTile
+    {
+        public MapTile(Game game)
+        {
+        }
+    }
+}
