41702b46409afb7a7239ada1ea12cf622e2205ad
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:28 +0200

    Create JTextureRegion.cs

---
+++ Graphics/Sprites/JTextureRegion.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Jolpango.Graphics.Sprites
+{
+    public class JTextureRegion
+    {
+        public Rectangle Region { get; set; }
+        public JTextureRegion(Texture2D texture, int width, int height, int index)
+        {
+            int tilesPerRow = texture.Width / width;
+            int xPos = (index % tilesPerRow) * width;
+            int yPos = (index / tilesPerRow) * height;
+
+            Region = new Rectangle(xPos, yPos, width, height);
+        }
+    }
+}
