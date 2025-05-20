f2cf6fc22fce8fda0795c32fa3ef5f31e0aad8d6
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:33:23 +0200

    Create JTimer.cs

---
+++ Utilities/JTimer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Utilities
+{
+    public delegate void MonoTimerCallback();
+    // UpdateSharedTimers needs to be called for shared timers to work
+    public class JTimer
+    {
+
+        private Action callback;
+        private float time;
+        public bool Done { get { return time < 0; } }
+        public JTimer(float time, Action callback)
+        {
+            this.time = time;
+            this.callback = callback;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            time -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (time < 0)
+            {
+                callback();
+            }
+        }
+    }
+}
