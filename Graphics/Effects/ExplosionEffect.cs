0bb117afc22f840df1e10e31d87c2d0e54e08116
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:10 +0200

    Create ExplosionEffect.cs

---
+++ Graphics/Effects/ExplosionEffect.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Jolpango.Graphics.Particles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Graphics.Effects
+{
+    public class ExplosionEffect
+    {
+    }
+}
