082d5df9304312793a37b45e7458b48a0486b7fd
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:31 +0200

    Create RotationTransition.cs

---
+++ Graphics/Transitions/RotationTransition.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Jolpango.Graphics.Particles;
+using MonoGame.Jolpango.Utilities;
+
+namespace MonoGame.Jolpango.Graphics.Transitions
+{
+    public class RotationTransition : IParticleTransition
+    {
+        public float StartRotation { get; set; } = 0.0f;
+        public float EndRotation { get; set; } = 1.0f;
+        public void Update(GameTime gameTime, float weight, Particle particle)
+        {
+            particle.Rotation = JMath.Lerp(StartRotation, EndRotation, weight);
+        }
+    }
+}
