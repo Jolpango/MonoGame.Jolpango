ce5e48079108b01285bbcb3b73159d2a4153b45a
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:32:26 +0200

    Create JSpriteSheet.cs

---
+++ Graphics/Sprites/JSpriteSheet.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Graphics.Sprites
+{
+    public class JSpriteSheet
+    {
+        public Texture2D Texture { get; set; }
+        private JTextureRegion[] jTextureRegions;
+        public int RegionWidth { get; set; }
+        public int RegionHeight { get; set; }
+        public JSpriteSheet(Texture2D texture, int regionWidth, int regionHeight)
+        {
+            Texture = texture;
+            RegionWidth = regionWidth;
+            RegionHeight = regionHeight;
+            int tilesWide = texture.Width / regionWidth;
+            int tilesHigh = texture.Height / regionHeight;
+            int numberOfTiles = tilesWide * tilesHigh;
+            jTextureRegions = new JTextureRegion[numberOfTiles];
+            for(int i = 0; i < numberOfTiles; i++)
+            {
+                jTextureRegions[i] = new JTextureRegion(texture, regionWidth, regionHeight, i);
+            }
+        }
+        public JTextureRegion GetRegion(int index)
+        {
+            return jTextureRegions[index];
+        }
+    }
+}
