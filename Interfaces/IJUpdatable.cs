d3634b1d973beaf386d6a08e39d93a355aa890a8
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:33:05 +0200

    Create IJUpdatable.cs

---
+++ Interfaces/IJUpdatable.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Interfaces
+{
+    public interface IJUpdatable
+    {
+        public void Update(GameTime gameTime);
+    }
+}
