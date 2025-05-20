1886115ab5b30430ec42a6f094bda681cd736821
Joel Funk Persson <joel.funkpersson@gmail.com>
2025-05-20 16:33:14 +0200

    Create MapLayer.cs

---
+++ Tiled/MapLayer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Jolpango.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Tiled
+{
+    public class MapLayer
+    {
+
+        public List<int> Data { get; set; }
+        public int Height { get; set; }
+        public double Opacity { get; set; }
+        public string Type { get; set; }
+        public bool Visible { get; set; }
+        public int Width { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
