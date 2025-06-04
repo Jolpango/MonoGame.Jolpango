5667ae2e93300701ca4caea65810d1b71c02b6f8
Joel Funk Persson <joel.funk.persson@connectitude.com>
2025-06-04 19:41:19 +0200

    Add inputclasses

---
+++ Input/JKeyboardInput.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Jolpango.Input
+{
+    public class JKeyboardInput
+    {
+        private KeyboardState previousState;
+        private KeyboardState currentState;
+
+        public bool IsUIFocused { get; set; } = false;
+
+        public JKeyboardInput()
+        {
+
+        }
+
+        public void Update()
+        {
+            previousState = currentState;
+            currentState = Keyboard.GetState();
+        }
+
+        public bool IsKeyPressed(Keys key)
+        {
+            return currentState.IsKeyDown(key) && previousState.IsKeyUp(key);
+        }
+
+        public bool IsKeyReleased(Keys key)
+        {
+            return currentState.IsKeyUp(key) && previousState.IsKeyDown(key);
+        }
+
+        public bool IsKeyDown(Keys key)
+        {
+            return currentState.IsKeyDown(key);
+        }
+        public bool IsKeyUp(Keys key)
+        {
+            return currentState.IsKeyDown(key);
+        }
+        public bool AreKeysDown(params Keys[] keys)
+        {
+            foreach (var key in keys)
+            {
+                if (currentState.IsKeyUp(key))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Input/JMouseInput.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace MonoGame.Jolpango.Input
+{
+    public class JMouseInput
+    {
+        private MouseState currentState;
+        private MouseState previousState;
+        public event Action<Vector2> MouseMoved;
+        public event Action<MouseState> LeftButtonClicked;
+        public event Action<MouseState> RightButtonClicked;
+        public event Action<MouseState> MiddleButtonClicked;
+
+        public Vector2 Position => currentState.Position.ToVector2();
+        public JMouseInput()
+        {
+        }
+
+        public void Update()
+        {
+            previousState = currentState;
+            currentState = Mouse.GetState();
+            if (currentState.Position != previousState.Position)
+            {
+                MouseMoved?.Invoke(currentState.Position.ToVector2());
+            }
+            if (IsLeftButtonClicked())
+            {
+                LeftButtonClicked?.Invoke(currentState);
+            }
+            if (IsRightButtonClicked())
+            {
+                RightButtonClicked?.Invoke(currentState);
+            }
+            if (IsMiddleButtonClicked())
+            {
+                MiddleButtonClicked?.Invoke(currentState);
+            }
+        }
+
+        public bool IsLeftButtonDown()
+        {
+            return currentState.LeftButton == ButtonState.Pressed;
+        }
+        public bool IsLeftButtonUp()
+        {
+            return currentState.LeftButton == ButtonState.Released;
+        }
+        public bool IsRightButtonDown()
+        {
+            return currentState.RightButton == ButtonState.Pressed;
+        }
+        public bool IsRightButtonUp()
+        {
+            return currentState.RightButton == ButtonState.Released;
+        }
+        public bool IsMiddleButtonDown()
+        {
+            return currentState.MiddleButton == ButtonState.Pressed;
+        }
+        public bool IsMiddleButtonUp()
+        {
+            return currentState.MiddleButton == ButtonState.Released;
+        }
+        public bool IsLeftButtonClicked()
+        {
+            return currentState.LeftButton == ButtonState.Pressed && previousState.LeftButton == ButtonState.Released;
+        }
+        public bool IsRightButtonClicked()
+        {
+            return currentState.RightButton == ButtonState.Pressed && previousState.RightButton == ButtonState.Released;
+        }
+        public bool IsMiddleButtonClicked()
+        {
+            return currentState.MiddleButton == ButtonState.Pressed && previousState.MiddleButton == ButtonState.Released;
+        }
+    }
+}
